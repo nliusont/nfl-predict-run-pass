14b9b97fdb6ef0210f630bf1761dff7da4902bc4
nliusont <nliusont@gmail.com>
2023-09-19 19:10:18 -0400

    commenting and cleanup

--- app.py
+++ app.py
@@ -5,10 +5,12 @@ import streamlit as st
 from app_funcs import get_game_data
 from app_funcs import predict_play
 from app_funcs import get_games
+from app_funcs import generate_play_text
 from datetime import date
 import nfl_data_py as nfl
-
 st.set_page_config(page_title="Predicting the next NFL play")
+
+### OPENING
 st.title("Predicting the next NFL play")
 st.write('Will the next play be a run or  pass? A question as old as time itself. \
          And now, there is an AI to predict it. We have finally answered humanity\'s biggest question.')
@@ -18,80 +20,73 @@ st.code('Is it accurate?', language=None)
 st.write('Sort of but not really. 70\% of the time, it works every time.')
 st.write('')
 
-week_1_end = date(2023, 9, 12)
+# get current week to generate list of historical weeks
+week_1_end = date(2023, 9, 12) # the Tuesday after week 1
 today = date.today()
 current_week = int((today - week_1_end).days / 7 + 2)
 week_list = ['Live'] + [f'Week {x}' for x in range(1, current_week)]
 
+### Model
 st.markdown('<h4>The ~Model~</h4>', unsafe_allow_html=True)
+
+# select live or previous week
 source = st.selectbox('Select source', options=week_list)
 if source:
+    # get list of all games historical or live
     games, game_ids, home_away_tms, week, game_data = get_games(source)
 
+# if games are returned, populate remaining fields
 if len(games) > 0:
     st.write('')
-    selected_game = st.selectbox(label='Select game', options=games)
-    selected_game_index = games.index(selected_game)
-    selected_tms = home_away_tms[selected_game_index]
-    selected_gameid = game_ids[selected_game_index]
+    selected_game = st.selectbox(label='Select game', options=games) # select game of interest
 
-    # select the game and, if not live game, the play #
+    # get home_away tuple and gameid (for API)
+    selected_game_index = games.index(selected_game) 
+    selected_tms = home_away_tms[selected_game_index] 
+    selected_gameid = game_ids[selected_game_index] 
+
+    # select the game and, if not live, the play #
     if selected_game:
         if source != 'Live':
             game_data = game_data[game_data['game_id']==selected_gameid].copy()
             num_of_plays = len(game_data)
             st.session_state['num_plays'] = num_of_plays
             selected_play = st.number_input(label='Select play', min_value=1, max_value=num_of_plays)
-            play_index = selected_play+1 # normally you'd subtract 1, but 0 is always a kickoff and 1 is game_start
+            play_index = selected_play+1 # 0 is game start and 1 is kickoff, so add 1
         else:
-            play_index = -1 #if the game is live, always go to the latest play
+            # if the game is live, always go to the latest play
+            play_index = -1 
         st.session_state['play_index'] = play_index
 
-
+    # initalize game series in session state
     if 'game_series' not in st.session_state:
         st.session_state['game_series'] = []
 
+    ### UPDATE GAME DATA
     update_button = st.button('Update game data')
     if update_button:
         st.session_state['update_button'] = 1
         get_game_data(source, week, selected_gameid, selected_tms, game_data)
-        posteam = st.session_state['posteam']
-        defteam = st.session_state['defteam']
-        game_series = st.session_state['game_series']
-        raw_game_data = st.session_state['raw_game_data']
-        down_distance_text = st.session_state['down_text']
-        home_team = selected_tms[0]
-        away_team = selected_tms[1]
+        generate_play_text(selected_tms)
 
-        if posteam == home_team:
-            home_score = game_series['posteam_score']
-            away_score = game_series['defteam_score']
-        else:
-            home_score = game_series['defteam_score']
-            away_score = game_series['posteam_score']
-
-        seconds = st.session_state['seconds']
-        sec_remainder = seconds % 60
-        min = (seconds / 60) - (sec_remainder/60)
-        quarter = st.session_state['quarter']
-        game_time = f'{min:.0f}:{int(sec_remainder):02} {quarter:.0f}Q'
-        
-        st.session_state['score_data'] = f'{away_team} {away_score:.0f}  {home_team} {home_score:.0f}'
-        st.session_state['has_the_ball'] =f'{posteam} has the ball'
-        st.session_state['game_time'] = game_time
-
+    # display game text
     if 'update_button' in st.session_state and st.session_state['update_button'] == 1:
         st.markdown(f"<h3 style='text-align: center;'>{st.session_state['score_data']}</h3>", unsafe_allow_html=True)
         st.markdown(f"<h4 style='text-align: center;'>{st.session_state['game_time']}</h4>", unsafe_allow_html=True)
         st.markdown(f"<h4 style='text-align: center;'>&#127944  <img src=\"{st.session_state['posteam_logo']}\" width=50></h4>", unsafe_allow_html=True)
         st.markdown(f"<h4 style='text-align: center;'>{st.session_state['down_text']}</h4>", unsafe_allow_html=True)
 
+    ### PREDICT
     if len(st.session_state['game_series'])>0:
         predict_button = st.button('Predict play', on_click=predict_play())
+
+        # get prediction
         if predict_button:
             pred = st.session_state['pred']
             st.markdown(f"<h1 style='text-align: center;'>{pred[0].upper()}</h1>", unsafe_allow_html=True)
             actual_play = st.session_state["actual_play"]
+
+            # API uses different play_type language
             if actual_play.startswith('Pass'):
                 outcome = 'pass'
             elif actual_play.startswith('Rush'):
@@ -103,6 +98,7 @@ if len(games) > 0:
             else: 
                 outcome = actual_play
 
+            # return eval against actual play
             if outcome == 'hasn\'t happened yet!':
                 emoji = '&#10067'
             elif pred==outcome:
@@ -114,6 +110,7 @@ if len(games) > 0:
 else:
     st.write('No live games! Try selecting a previous week')
 
+### FOOTER
 st.markdown('<p></p>', unsafe_allow_html=True)
 st.markdown('<p></p>', unsafe_allow_html=True)
 st.markdown('<p></p>', unsafe_allow_html=True)
--- app_funcs.py
+++ app_funcs.py
@@ -1,4 +1,7 @@
 def get_ordinal(number):
+    '''
+    Takes a number and returns the ordinal as a string
+    '''
     if 10 <= number % 100 <= 20:
         suffix = 'th'
     else:
@@ -6,6 +9,17 @@ def get_ordinal(number):
     return f"{number:.0f}{suffix}"
 
 def get_games(source):
+    '''
+    Takes in the source as Live or Week #
+    Retrieves a list of games either from API or historical data
+    Returns 
+        list of games
+        list of game ids
+        list of tuples with (home_team, away_team)
+        week #
+        raw game data (to reduce re-querying)
+    '''
+
     import requests
     import json
 
@@ -14,17 +28,18 @@ def get_games(source):
         r = requests.get(url)
         d = json.loads(r.text)
 
-        # live events
-        # get game that are in progress (2)
+        # get game that are in progress (status.startswith 2)
         games = [x['shortName'] for x in d['events'] if (x['status']['type']['id'].startswith('2'))]
         game_ids = [x['id'] for x in d['events'] if (x['status']['type']['id'].startswith('2'))]
         home_away_tms = [(x.split('@ ')[1], x.split(' @')[0]) for x in games]
         week = d['week']['number']
 
         return games, game_ids, home_away_tms, week, d
+    
+    # if historical game
     else:
         import nfl_data_py as nfl
-        week = int(source[-1])
+        week = int(source[-1]) # get week num
         data = nfl.import_pbp_data([2023], 
                            downcast=True, 
                            cache=False, 
@@ -47,26 +62,37 @@ def get_games(source):
                                     'play_type',
                                     'game_id'
                                     ])
+        
+        # filter to selected week
         data = data[data['week']==week].copy()
+
+        # get unique list of games
         game_df = data[['home_team', 'away_team', 'game_id']].drop_duplicates()
+
         home_away_tms = tuple(zip(game_df['home_team'], game_df['away_team']))
         games = list(game_df["away_team"].astype(str) + ' @ ' + game_df["home_team"].astype(str))
         game_ids = game_df['game_id'].to_list()
+
         return games, game_ids, home_away_tms, week, data
 
 def get_game_data(source, week, gameid, teams, game_data):
+    '''
+    Produce play data based on the source, week and game
+    Intakes game_data to prevent requerying for historical data
+    Writes play information and game_series to session state 
+    for display and input to prediction
+    '''
     import pandas as pd
     import streamlit as st
 
+    # get selected play
     play_index = st.session_state['play_index']
 
     if source == 'Live':
         import requests
         import json
-        url = 'http://site.api.espn.com/apis/site/v2/sports/football/nfl/scoreboard'
-        r = requests.get(url)
-        d = json.loads(r.text)
 
+        # query API for specific game
         url = f'https://sports.core.api.espn.com/v2/sports/football/leagues/nfl/events/{gameid}3/competitions/{gameid}/plays?limit=300'
         r = requests.get(url)
         d = json.loads(r.text)
@@ -81,11 +107,11 @@ def get_game_data(source, week, gameid, teams, game_data):
             game_seconds_remaining = seconds
             half_seconds_remaining = seconds
         else:
-            game_pad = 15*60*(4-quarter) # there are 3x 15 min halves remaining in the game
+            game_pad = 15*60*(4-quarter) 
             game_seconds_remaining = seconds + game_pad
 
             if quarter < 3:
-                half_pad = 15*60*(2-quarter) # there is one 15 min quarter remaining in the half
+                half_pad = 15*60*(2-quarter) 
                 half_seconds_remaining = seconds+half_pad
             if quarter > 2:
                 half_seconds_remaining = game_seconds_remaining
@@ -93,6 +119,7 @@ def get_game_data(source, week, gameid, teams, game_data):
         down = d['items'][play_index]['end']['down']
         ydstogo = d['items'][play_index]['end']['distance']
         yardline = d['items'][play_index]['end']['yardsToEndzone']
+
         if ydstogo >= yardline:
             goal_to_go = 1.
         else:
@@ -110,12 +137,15 @@ def get_game_data(source, week, gameid, teams, game_data):
 
             actual_play = d['items'][play_index+1]['type']['text']
 
+            # API does not have a field for posessing team
+            # it only provides the url for the team
             url = d['items'][play_index]['team']['$ref']
             r = requests.get(url)
             d = json.loads(r.text)
-
             pos_team = d['abbreviation']
             def_team = teams[teams.index(pos_team) - 1]
+
+            # assign scores by home or away
             if pos_team == teams[0]:
                 pos_score = home_score
                 def_score = away_score
@@ -125,6 +155,7 @@ def get_game_data(source, week, gameid, teams, game_data):
                 def_score = home_score
                 is_pos_home = 0
 
+            # create index for series that will be predicted from
             index = ['week',
                     'yardline_100',
                     'half_seconds_remaining',
@@ -136,18 +167,19 @@ def get_game_data(source, week, gameid, teams, game_data):
                     'defteam_score',
                     'is_pos_home',]
 
-        data = [week,
-                yardline,
-                half_seconds_remaining,
-                game_seconds_remaining,
-                down,
-                goal_to_go,
-                ydstogo,
-                pos_score,
-                def_score,
-                is_pos_home]
+            # organize data for series
+            data = [week,
+                    yardline,
+                    half_seconds_remaining,
+                    game_seconds_remaining,
+                    down,
+                    goal_to_go,
+                    ydstogo,
+                    pos_score,
+                    def_score,
+                    is_pos_home]
 
-        series = pd.Series(data, index=index)
+            series = pd.Series(data, index=index)
 
 
     else:
@@ -167,15 +199,21 @@ def get_game_data(source, week, gameid, teams, game_data):
                             'defteam_score',
                             'play_type'
                             ]]
+        
+        # located selected play
         play_data = game_data.iloc[play_index]
+
+        # create is_pos_home col
         home_team = teams[0]
         play_data['is_pos_home'] = 0
         if play_data['posteam']==home_team:
             play_data['is_pos_home'] == 1
 
+        # create down distance text
         down_text = get_ordinal(play_data['down'])
         down_distance_text = f'{down_text} & {play_data["ydstogo"]:.0f}'
 
+        # assign vars for writing to session_state
         pos_team = play_data['posteam']
         def_team = play_data['defteam']
         raw_game_data = play_data
@@ -184,6 +222,7 @@ def get_game_data(source, week, gameid, teams, game_data):
         seconds = play_data['game_seconds_remaining'] - (15*60*quarters_left)
         actual_play = play_data['play_type']
 
+        # create series for prediction
         series = play_data[['week',
                             'yardline_100',
                             'half_seconds_remaining',
@@ -195,9 +234,9 @@ def get_game_data(source, week, gameid, teams, game_data):
                             'defteam_score',
                             'is_pos_home',]]
         
-
     logo = f'https://a.espncdn.com/i/teamlogos/nfl/500-dark/scoreboard/{pos_team}.png'
 
+    # identify states to write to session_state
     states = {'posteam':pos_team, 
             'defteam':def_team, 
             'game_series':series,
@@ -210,26 +249,71 @@ def get_game_data(source, week, gameid, teams, game_data):
     
     for s, v in states.items():
         st.session_state[s] = v
+
+def generate_play_text(selected_tms):
+    '''
+    Reads from session state and generates display text for play information
+    Writes play text to session_state
+    '''
+
+    import streamlit as st
+
+    # retrieve data from session state
+    posteam = st.session_state['posteam']
+    defteam = st.session_state['defteam']
+    game_series = st.session_state['game_series']
+    raw_game_data = st.session_state['raw_game_data']
+    down_distance_text = st.session_state['down_text']
+    home_team = selected_tms[0]
+    away_team = selected_tms[1]
+
+    # 
+    if posteam == home_team:
+        home_score = game_series['posteam_score']
+        away_score = game_series['defteam_score']
+    else:
+        home_score = game_series['defteam_score']
+        away_score = game_series['posteam_score']
+
+    # game clock
+    seconds = st.session_state['seconds']
+    sec_remainder = seconds % 60
+    min = (seconds / 60) - (sec_remainder/60)
+    quarter = st.session_state['quarter']
+    game_time = f'{min:.0f}:{int(sec_remainder):02} {quarter:.0f}Q'
+    
+    st.session_state['score_data'] = f'{away_team} {away_score:.0f}  {home_team} {home_score:.0f}'
+    st.session_state['has_the_ball'] = f'{posteam} has the ball'
+    st.session_state['game_time'] = game_time
     
 def predict_play():
+    '''
+    Reads games series from session_state
+    Loads RF model
+    Predicts play_type
+    '''
+
     import pandas as pd
     import pickle
     import streamlit as st
     import numpy as np
     import pickle
 
+    # read in data
     game_series = st.session_state['game_series']
     posteam = st.session_state['posteam']
     defteam = st.session_state['defteam']
 
+    # import feature cols used for training
     with open('data/feature_cols.pkl', 'rb') as f:
         input_cols = pickle.load(f)
     
+    # get list of all cols and create if missing
     missing_cols = list(set(input_cols) - set(game_series.index))
-
     for c in missing_cols:
         game_series[c] = 0
 
+    # find proper dummy cols and fill with 1 (e.g. posteam_CIN if CIN posseses the ball)
     pos_team_col = f'posteam_{posteam}'
     def_team_col = f'defteam_{defteam}'
     game_series[pos_team_col] = 1
@@ -243,6 +327,7 @@ def predict_play():
     
     pred = model.predict(input_data)
 
+    # write to session state
     st.session_state['pred'] = pred
 
 
